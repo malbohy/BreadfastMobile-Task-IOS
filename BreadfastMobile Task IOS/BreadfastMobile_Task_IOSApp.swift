63e798b083144f60cd50f03ab6b0557462ea2881
mohamed elbohy <malbohy@gmail.com>
2023-04-09 18:46:54 +0200

    Initial Commit

---
+++ BreadfastMobile Task IOS/BreadfastMobile_Task_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreadfastMobile_Task_IOSApp.swift
+//  BreadfastMobile Task IOS
+//
+//  Created by mohamed albohy on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BreadfastMobile_Task_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreadfastMobile Task IOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BreadfastMobile Task IOS
+//
+//  Created by mohamed albohy on 09/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
